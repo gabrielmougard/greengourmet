74ae9aee1ae2c393acfab7ecd55d733b5e41d23d
gabrielmougard <gabriel.mougard@gmail.com>
2020-03-21 22:38:25 +0100

    add some frontend related to scanner

--- src/frontend/app/src/components/home/Scanner.js
+++ src/frontend/app/src/components/home/Scanner.js
@@ -73,7 +73,11 @@ const Scanner = props => {
     // If you do not specify a target,
     // QuaggaJS would look for an element that matches
     // the CSS selector #interactive.viewport
-    <div id="interactive" className="viewport" />
+    <>
+    <div id="interactive" className="viewport">
+    </div>
+    <div className="scanner-result"></div>
+    </>
   );
 };
 
