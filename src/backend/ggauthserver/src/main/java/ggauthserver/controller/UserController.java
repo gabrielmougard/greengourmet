7099c21067f3a64abf57f4925093c6deca0470ec
gabrielmougard <gabriel.mougard@gmail.com>
2020-03-27 13:04:20 +0100

    google authentication is working

--- src/backend/ggauthserver/src/main/java/ggauthserver/controller/UserController.java
+++ src/backend/ggauthserver/src/main/java/ggauthserver/controller/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/console/me")
+    @GetMapping("/user/me")
     @PreAuthorize("hasRole('USER')")
     public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
         return userRepository.findById(userPrincipal.getId())
