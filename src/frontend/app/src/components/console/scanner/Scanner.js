5d74b773b49674ac73b35786ed9c2d1e244199c0
gabrielmougard <gabriel.mougard@gmail.com>
2020-04-03 14:10:07 +0200

    add loader when reliable bar code decoded

--- src/frontend/app/src/components/console/scanner/Scanner.js
+++ src/frontend/app/src/components/console/scanner/Scanner.js
@@ -5,13 +5,25 @@ import Quagga from "quagga";
 
 import Button from '@material-ui/core/Button';
 import Drawer from '@material-ui/core/Drawer';
-
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
 
 //CSS
 import './Scanner.css'
 
+
+const backdropStyles = makeStyles((theme) => ({
+    backdrop: {
+      zIndex: theme.zIndex.drawer + 1,
+      color: '#fff',
+    },
+  }));
+
+
 const Scanner = props => {
     const [drawerState, setDrawerState] = React.useState(false);
+    const [backDropState, setBackDropState] = React.useState(false);
 
   useEffect(() => {
     Quagga.init(config, err => {
@@ -79,6 +91,7 @@ const Scanner = props => {
         var decoded = detectionThreshold(existing)
         if (decoded) {
             console.log("A reliable decoded value is : "+decoded);
+            setBackDropState(true);
             //call the saga here for getting the content of the decoded barcode
             //TODO :
             //
@@ -102,7 +115,11 @@ const Scanner = props => {
     setDrawerState(open);
   }
 
+  const classBackdrop = backdropStyles();
 
+  const handleCloseBackdrop = () => {
+      setBackDropState(false);
+  }
 
   return (
     // If you do not specify a target,
@@ -112,7 +129,11 @@ const Scanner = props => {
     <div id="interactive" className="viewport"></div>
     <Drawer anchor={"bottom"} open={drawerState} onClose={toggleDrawer(false)}>
     </Drawer>
+    <Backdrop className={classBackdrop.backdrop} open={backDropState} onClick={handleCloseBackdrop}>
+        <CircularProgress color="inherit" />
+    </Backdrop>
     </>
+    
   );
 };
 
